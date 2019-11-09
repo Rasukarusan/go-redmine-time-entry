33d83812e52badc62d2d28a76d18f72abda97811
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-11-09 18:59:11 +0900

    時間記録までを実装

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"github.com/Rasukarusan/go-redmine-time-entry/config"
+	"github.com/Rasukarusan/go-redmine-time-entry/domain"
 	"github.com/Rasukarusan/go-redmine-time-entry/infrastructure"
 	"github.com/sclevine/agouti"
 )
 
 func main() {
+
+	c := config.Load()
+
 	driver := infrastructure.Driver
 	driver.Start()
 	defer driver.Stop()
 
+	// ログイン
 	page, _ := driver.NewPage(agouti.Browser("chrome"))
-	page.Navigate("http://qiita.com/")
+	page.Navigate(c.Url + "/login")
+	page.FindByID("username").SendKeys(c.UserId)
+	page.FindByID("password").SendKeys(c.Password)
+	page.FindByName("login").Submit()
+
+	// 登録内容を取得
+	timeEntry := domain.TimeEntry{
+		SpentOn:    "2019-11-01",
+		Hour:       "hoge",
+		Comment:    "hoge",
+		ActivityID: "調査作業",
+	}
+
+	// 時間記録
+	page.Navigate(c.Url + "/issues/59865/time_entries/new")
+	page.FindByID("")
+	page.FindByID("time_entry_spent_on").Clear()
+	page.FindByID("time_entry_spent_on").SendKeys(timeEntry.SpentOn)
+	page.FindByID("time_entry_hours").SendKeys(timeEntry.Hour)
+	page.FindByID("time_entry_comments").SendKeys(timeEntry.Comment)
+	page.FindByID("time_entry_activity_id").Select(timeEntry.ActivityID)
+	page.FindByName("commit").Click()
+
 }
